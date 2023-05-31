4d9e52c3ef8bcd16946ae3233f2df49007cbc981
Raphaël Fosse <raphael.fosse@isen-ouest.yncrea.fr>
2023-05-31 11:59:11 +0200

    data.sql

---
+++ data.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.user(prenom_user, nom_user, age_user, mail_user, mot_de_passe) VALUES('rapahel', 'fosse', 19, 'raphael.fosse@isen-ouest.yncrea.fr', 'mdp');
+
+INSERT INTO public.user(prenom_user, nom_user, age_user, mail_user, mot_de_passe) VALUES('maxime', 'nellenger', 19, 'maxime.bellenger@isen-ouest.yncrea.fr', 'mdp');
+
+INSERT INTO artiste(nom_artiste, type_artiste)
