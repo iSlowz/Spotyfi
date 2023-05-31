bb4ba419c07654ef47d4e703d6458e646a57a961
Kaioox <ethan.lepan@gmail.com>
2023-05-31 16:04:12 +0200

    sign up css html finito

---
+++ Sign_up.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+    <!-- Google Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+    <!-- Custom CSS -->
+    
+   
+    <link rel="stylesheet" href="style/Sign_up.css">
+
+    <title>Spotyfi</title>
+
+</head>
+
+<body>
+
+  <div class="form">
+    
+    <nav class="navbar">
+      <div class="container-fluid align-content-center justify-content-center">
+      <a>Spotyfi++</a>
+      </div>
+    </nav>
+
+    <div class="rectangle-Sign_up">
+
+      <div class="login-form">
+        <form action="Login.php" method="post" class="g-3" id="LoginForm">
+          
+          <div class="mb-3">
+
+            <table width="100%" cellspacing="0" cellpadding="5">
+              <tr>
+                <td colspan="2"><label class="trait">Sign up</label></td>
+              </tr>
+              
+              <tr>
+                <td>
+                  <div class="texte-gauche">
+                    <label for="userInput" class="form-label">Nom :</label>
+                    <input type="text" class="form-control" id="userInput" aria-describedby="userInput" name="userInput">
+
+                    <label for="userInput" class="form-label">Prénom :</label>
+                    <input type="text" class="form-control" id="userInput" aria-describedby="userInput" name="userInput">
+                    
+                    <label for="userInput" class="form-label">Age :</label>
+                    <input type="text" class="form-control" id="userInput" aria-describedby="userInput" name="userInput">
+                  </div>
+                </td>
+                
+                <td>
+                  <div class="texte-droit">
+                    <label for="userInput" class="form-label">Email :</label>
+                    <input type="text" class="form-control" id="userInput" aria-describedby="userInput" name="userInput">
+
+                    <label for="userInput" class="form-label">Mot de passe :</label>
+                    <input type="text" class="form-control" id="userInput" aria-describedby="userInput" name="userInput">
+                    
+                    <label for="userInput" class="form-label">Vérification mot de passe :</label>
+                    <input type="text" class="form-control" id="userInput" aria-describedby="userInput" name="userInput">
+                  </div>
+                </td>
+              </tr>
+            </table>
+          </div>
+          <button type="button" class="btn">S'inscrire</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
