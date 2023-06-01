c076f18e81e01d2f7c3b4684e2e5f21df887109d
iSlowz <mbellenger118@gmail.com>
2023-06-01 16:57:56 +0200

    p

--- request.php
+++ request.php
@@ -1,3 +1,22 @@
 <?php
-    
+require_once('')
+
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+$request = substr($_SERVER['PATH_INFO'], 1);
+$request = explode('/', $request);
+$requestRessource = array_shift($request);
+
+$id = array_shift($request);
+if ($id == '')
+    $id = NULL;
+$result = null;
+
+switch ($requestRessource) {
+    case 'musique':
+        switch ($requestMethod){
+            case "GET":
+
+        }
+}
+
 ?>
