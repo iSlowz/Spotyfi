9e6e552d380aa43316294911dd5599497cc4ed69
Raphaël Fosse <raphael.fosse@isen-ouest.yncrea.fr>
2023-06-01 16:47:26 +0200

    data update

--- sql/data.sql
+++ sql/data.sql
@@ -30,5 +30,9 @@ INSERT INTO public.playlist(titre_playlist, date_creation_playlist, id_user) VAL
 INSERT INTO public.musique_playlist(id_musique, id_playlist, date_ajout_musique_playlist) VALUES((SELECT id_musique FROM public.musique WHERE titre_musique = 'music1'), (SELECT id_playlist FROM public.playlist WHERE titre_playlist = 'printemps'), NOW());
 INSERT INTO public.musique_playlist(id_musique, id_playlist, date_ajout_musique_playlist) VALUES((SELECT id_musique FROM public.musique WHERE titre_musique = 'music2'), (SELECT id_playlist FROM public.playlist WHERE titre_playlist = 'printemps'), NOW());
 
-INSERT INTO playlist(titre_playlist, date_creation_playlist, id_user)
-VALUES (:titre,:date, :id);
+
+INSERT INTO public.playlist(titre_playlist, date_creation_playlist, id_user) VALUES('Historique', NOW(), (SELECT id_user FROM public.users WHERE mail_user = 'aa@isen.fr'));
+INSERT INTO public.musique_playlist(id_musique, id_playlist, date_ajout_musique_playlist) VALUES((SELECT id_musique FROM public.musique WHERE titre_musique = 'music1'), (SELECT id_playlist FROM public.playlist WHERE titre_playlist = 'Historique' AND id_user = (SELECT id_user FROM public.users WHERE mail_user = 'aa@isen.fr')), NOW());
+INSERT INTO public.musique_playlist(id_musique, id_playlist, date_ajout_musique_playlist) VALUES((SELECT id_musique FROM public.musique WHERE titre_musique = 'music2'), (SELECT id_playlist FROM public.playlist WHERE titre_playlist = 'Historique' AND id_user = (SELECT id_user FROM public.users WHERE mail_user = 'aa@isen.fr')), NOW());
+INSERT INTO public.musique_playlist(id_musique, id_playlist, date_ajout_musique_playlist) VALUES((SELECT id_musique FROM public.musique WHERE titre_musique = 'music3'), (SELECT id_playlist FROM public.playlist WHERE titre_playlist = 'Historique' AND id_user = (SELECT id_user FROM public.users WHERE mail_user = 'aa@isen.fr')), NOW());
+
