ab26b9d4f220421a3d755f3da2bd3b54304956bf
Raphaël Fosse <raphael.fosse@isen-ouest.yncrea.fr>
2023-06-01 08:40:56 +0200

    ab user

--- sql/data.sql
+++ sql/data.sql
@@ -2,8 +2,9 @@
 INSERT INTO public.users(prenom_user, nom_user, age_user, mail_user, mot_de_passe) VALUES('raphael', 'fosse', 19, 'raphael.fosse@isen-ouest.yncrea.fr', 'mdp');
 INSERT INTO public.users(prenom_user, nom_user, age_user, mail_user, mot_de_passe) VALUES('maxime', 'bellenger', 19, 'maxime.bellenger@isen-ouest.yncrea.fr', 'mdp');
 INSERT INTO public.users(prenom_user, nom_user, age_user, mail_user, mot_de_passe) VALUES('ethan', 'le-pan', 19, 'ethan.le-pan@isen-ouest.yncrea.fr', 'mdp');
-INSERT INTO public.users(prenom_user, nom_user, age_user, mail_user, mot_de_passe) VALUES('dorian', 'rena', 19, 'dorian.rena@isen-ouest.yncrea.fr', 'mdp');
-
+INSERT INTO public.users(prenom_user, nom_user, age_user, mail_user, mot_de_passe) VALUES('dorian', 'rena', 19, 'dorian.rena@isen-ouest.yncrea.fr', 'mdp');*/
+INSERT INTO public.users(prenom_user, nom_user, age_user, mail_user, mot_de_passe) VALUES('a', 'b', 19, 'ab@isen.fr', 'mdp');
+/*
 INSERT INTO public.styles(nom_style) VALUES('pop');
 INSERT INTO public.styles(nom_style) VALUES('rock');
 INSERT INTO public.styles(nom_style) VALUES('funk');
@@ -24,4 +25,5 @@ INSERT INTO public.playlist(titre_playlist, date_creation_playlist, id_user) VAL
 
 INSERT INTO public.musique_playlist(id_musique, id_playlist, date_ajout_musique_playlist) VALUES((SELECT id_musique FROM public.musique WHERE titre_musique = 'music1'), (SELECT id_playlist FROM public.playlist WHERE titre_playlist = 'printemps'), NOW());
 INSERT INTO public.musique_playlist(id_musique, id_playlist, date_ajout_musique_playlist) VALUES((SELECT id_musique FROM public.musique WHERE titre_musique = 'music2'), (SELECT id_playlist FROM public.playlist WHERE titre_playlist = 'printemps'), NOW());
+
 */
