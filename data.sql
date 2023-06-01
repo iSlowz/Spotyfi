25abab9690f060a8ae54799718ec61bb267b9534
Raphaël Fosse <raphael.fosse@isen-ouest.yncrea.fr>
2023-06-01 08:22:06 +0200

    organisation update

--- data.sql
+++ data.sql
@@ -23,7 +23,7 @@ INSERT INTO public.musique(titre_musique, lien_musique, duree_musique, date_paru
 INSERT INTO public.musique(titre_musique, lien_musique, duree_musique, date_parution_musique, id_artiste, id_album, id_style) VALUES('music3', '../musique/ukulele-trip.mp3', TIME '00:01:00', DATE '2023-08-08', (SELECT id_artiste FROM public.artiste WHERE nom_artiste = 'M'), (SELECT id_album FROM public.album WHERE title_album = 'second_album'), (SELECT id_style FROM public.album WHERE title_album = 'second_album'));
 
 INSERT INTO public.playlist(titre_playlist, date_creation_playlist, id_user) VALUES('printemps', NOW(), (SELECT id_user FROM public.users WHERE mail_user = 'raphael.fosse@isen-ouest.yncrea.fr'));
-*/
 
 INSERT INTO public.musique_playlist(id_musique, id_playlist, date_ajout_musique_playlist) VALUES((SELECT id_musique FROM public.musique WHERE titre_musique = 'music1'), (SELECT id_playlist FROM public.playlist WHERE titre_playlist = 'printemps'), NOW());
 INSERT INTO public.musique_playlist(id_musique, id_playlist, date_ajout_musique_playlist) VALUES((SELECT id_musique FROM public.musique WHERE titre_musique = 'music2'), (SELECT id_playlist FROM public.playlist WHERE titre_playlist = 'printemps'), NOW());
+*/
