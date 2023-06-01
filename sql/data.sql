595587f9b730ddc5a4e348bb7ac67df289aeb1c9
Raphaël Fosse <raphael.fosse@isen-ouest.yncrea.fr>
2023-06-01 08:35:05 +0200

    sql directory

--- sql/data.sql
+++ sql/data.sql
@@ -13,10 +13,8 @@ INSERT INTO public.artiste(nom_artiste, type_artiste) VALUES('M', 'chanteur');
 INSERT INTO public.artiste(nom_artiste, type_artiste) VALUES('N', 'chanteur');
 INSERT INTO public.artiste(nom_artiste, type_artiste) VALUES('L', 'groupe');
 
-
 INSERT INTO public.album(title_album, photo_album, date_creation_album, id_artiste, id_style) VALUES('first_album', '../album_cover/album1.png', DATE '2022-08-08', (SELECT id_artiste FROM public.artiste WHERE nom_artiste = 'M'), (SELECT id_style FROM public.style WHERE nom_style = 'pop'));
 INSERT INTO public.album(title_album, photo_album, date_creation_album, id_artiste, id_style) VALUES('second_album', '../album_cover/album2.png', DATE '2023-08-08', (SELECT id_artiste FROM public.artiste WHERE nom_artiste = 'M'), (SELECT id_style FROM public.style WHERE nom_style = 'rock'));
-
 
 INSERT INTO public.musique(titre_musique, lien_musique, duree_musique, date_parution_musique, id_artiste, id_album, id_style) VALUES('music1', '../musique/epic-power.mp3', TIME '00:01:01', DATE '2022-08-08', (SELECT id_artiste FROM public.artiste WHERE nom_artiste = 'M'), (SELECT id_album FROM public.album WHERE title_album = 'first_album'), (SELECT id_style FROM public.album WHERE title_album = 'first_album'));
 INSERT INTO public.musique(titre_musique, lien_musique, duree_musique, date_parution_musique, id_artiste, id_album, id_style) VALUES('music2', '../musique/epic-trailer.mp3', TIME '00:00:56', DATE '2022-08-08', (SELECT id_artiste FROM public.artiste WHERE nom_artiste = 'M'), (SELECT id_album FROM public.album WHERE title_album = 'first_album'), (SELECT id_style FROM public.album WHERE title_album = 'first_album'));
