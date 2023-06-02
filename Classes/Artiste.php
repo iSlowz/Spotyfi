b52b711cf218a47de05590f9e032e214ec2deda5
Kaioox <ethan.lepan@gmail.com>
2023-06-02 11:58:43 +0200

    artiste

--- Classes/Artiste.php
+++ Classes/Artiste.php
@@ -15,7 +15,7 @@ class Artiste
     static function getArtiste($id_artiste){
         try {
             $conn = Database::connexionBD();
-            $statement = $conn->prepare("SELECT * FROM artiste WHERE id_artiste:id_artiste");
+            $statement = $conn->prepare("SELECT * FROM artiste WHERE id_artiste=:id_artiste");
             $statement->bindParam(':id_artiste', $id_artiste);
             $statement->execute();
             return $statement->fetch();
@@ -24,6 +24,21 @@ class Artiste
             return false;
         }
     }   
+
+    static function getArtisteBySearch($artiste){
+
+        try{
+            $conn = Database::connexionDB();
+            $statement = $conn->prepare("SELECT pseudo_artiste, id_artiste FROM artiste WHERE pseudo_artiste ILIKE '%:artiste%'");
+            $statement->bindParam(':artiste', $artiste);
+            $statement->execute();
+            return $statement->fetch();
+        } catch (PDOException $exception) {
+            error_log('Connection error: '.$exception->getMessage());
+            return false;
+        }
+
+    }
 
 }
 
