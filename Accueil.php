a6bc3d30385462c97aa86abe730ffeb4bbd8beac
Kaioox <ethan.lepan@gmail.com>
2023-06-02 10:59:23 +0200

    accueil bouton user finito

--- Accueil.php
+++ Accueil.php
@@ -53,8 +53,9 @@ $id_user = Utilisateur::Connexion()
         color : white;
 
         margin-left: 5%;
-        padding-right: 10%;
 
+        width : 500px;
+    
       }
 
       #bouton-recherche{
@@ -62,13 +63,13 @@ $id_user = Utilisateur::Connexion()
         border : solid white;
         background-color: rgb(25, 157, 166);
         color : white;
-        
-      }
+
+      }  
 
       .bouton-user{
 
         margin-left: auto;
-
+        margin-right: 2%;
 
       }
 
@@ -103,7 +104,7 @@ echo "<div id='id_user' style='display: none'>".$id_user."</div>";
     <div class="Navbar-Recherche">
       <div class="flex-recherche">
         <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Recherche" aria-label="Search" id="bar-recherche">
+          <input class="form-control me-2" id="bar-recherche" type="search" placeholder="Recherche" aria-label="Search">
 
           <!-- Bouton de recherche trouvé sur bootstrap -->
 
@@ -115,7 +116,12 @@ echo "<div id='id_user' style='display: none'>".$id_user."</div>";
         </form>
       </div>
       <div class="bouton-user">
-        <button class="btn" id="id-bouton-user" type="submit">User</button>
+        <button class="btn" id="id-bouton-user" type="submit"> User
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+          </svg> 
+        </button>
       </div>
     </div>
 
