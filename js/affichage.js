8447f577889a224fe6127b344fed496fdb235678
Raphaël Fosse <raphael.fosse@isen-ouest.yncrea.fr>
2023-06-06 11:21:33 +0200

    update

--- js/affichage.js
+++ js/affichage.js
@@ -712,6 +712,11 @@ function showArtiste(artiste) {
 
 }
 
+
+/*--------------------------------------------------------------------------------------------------------------*/
+/* Fonctions relié à la balise audio */
+/*--------------------------------------------------------------------------------------------------------------*/
+
 let playing = false;
 function playPauseFrom(lien){
     document.getElementById('player').innerHTML = '<source src="' + lien + '" >';
@@ -767,6 +772,10 @@ function updateMusiqueBar(){
     console.log(musiqueBar.value);
 }
 
+function like(){
+    
+}
+
 function boucle(){
     if(document.getElementById('player').loop == true){
         console.log('false');
@@ -779,6 +788,7 @@ function boucle(){
         document.getElementById('btn-boucle').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-repeat-1" viewBox="0 0 16 16"> <path d="M11 4v1.466a.25.25 0 0 0 .41.192l2.36-1.966a.25.25 0 0 0 0-.384l-2.36-1.966a.25.25 0 0 0-.41.192V3H5a5 5 0 0 0-4.48 7.223.5.5 0 0 0 .896-.446A4 4 0 0 1 5 4h6Zm4.48 1.777a.5.5 0 0 0-.896.446A4 4 0 0 1 11 12H5.001v-1.466a.25.25 0 0 0-.41-.192l-2.36 1.966a.25.25 0 0 0 0 .384l2.36 1.966a.25.25 0 0 0 .41-.192V13h6a5 5 0 0 0 4.48-7.223Z"/> <path d="M9 5.5a.5.5 0 0 0-.854-.354l-1.75 1.75a.5.5 0 1 0 .708.708L8 6.707V10.5a.5.5 0 0 0 1 0v-5Z"/> </svg>';
     }
 }
+
 function getDuration(){
     let x = document.getElementById('player');
     x.play();
@@ -794,6 +804,7 @@ function setCurrentTime(k){
     x.play();
     x.currentTime = k;
 }
+
 function plus5s(){
     let x = document.getElementById('player');
     x.play();
