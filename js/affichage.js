3dd04432801e8a8c1a748330a7c5abcd6cde839f
Kaioox <ethan.lepan@gmail.com>
2023-06-06 13:41:17 +0200

    css

--- js/affichage.js
+++ js/affichage.js
@@ -397,11 +397,20 @@ function showMusique(musique) {
             '<p> Son album : <button type="button" class="album-bouton" value="' + musique["id_album"] + '">' + musique["titre_album"] + '</button></p>' +
             '<p> Son artiste : <button type="button" class="artiste-bouton" value="' + musique["id_artiste"] + '">' + musique["pseudo_artiste"] + '</button></p>' +
             '<p> Style : ' + musique["nom_style"] + '</p>' +
-            '</div>')
+            '</div>'
+        
+            )
+        
         if (musique["like"]===false) {
-            $(".flex-page").append('<button type="button" class="like-musique" value="' + musique["id_musique"] + '"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">' +
-                '<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>' +
-                '</svg></button>')
+            $(".flex-page").append(
+                
+                '<button type="button" class="like-musique like-recherche-page" value="' + musique["id_musique"] + '">' + 
+                  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">' +
+                    '<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>' +
+                  '</svg>'+
+                '</button>'
+            
+            )
             $(".like-musique").click(function (event) {
                 let id = $(event.target).closest('.like-musique').attr('value')
                 console.log(id);
@@ -410,10 +419,17 @@ function showMusique(musique) {
                 },"user="+id_user)
             })
         }
+
         else{
-            $(".flex-page").append('<button type="button" class="unlike-musique" value="' + musique["id_musique"] + '"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">'+
-                '<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>'+
-                '</svg></button>')
+            $(".flex-page").append(
+                
+                '<button type="button" class="unlike-musique" value="' + musique["id_musique"] + '">'+
+                  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">'+
+                    '<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>'+
+                  '</svg>'+
+                '</button>'
+                
+            )
             $(".unlike-musique").click(function (event) {
                 let id = $(event.target).closest('.unlike-musique').attr('value')
                 console.log(id);
@@ -422,18 +438,27 @@ function showMusique(musique) {
                 },)
             })
         }
-        $(".flex-page").append('<button type="button" id="add-playlist" value="'+musique["id_musique"]+'">' +
-            '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">' +
-            '  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>' +
-            '  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>' +
-            '</svg>' +
-            '</button>')
-        $(".flex-page").append('<button type="button" class="play-musique" value="' + musique["id_musique"] + '" onClick="playPauseFrom(\'' + musique["lien_musique"] + '\' )">' +
-            '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">'+
-            '<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>'+
-            '<path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>'+
-            '</svg>' +
-            '</button>')
+
+        $(".flex-page").append(
+            
+            '<button type="button" class="add-playlist" id="add-playlist" value="'+musique["id_musique"]+'">' +
+              '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">' +
+                '<path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>' +
+                '<path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>' +
+              '</svg>' +
+            '</button>'
+        
+        )
+        $(".flex-page").append(
+            
+            '<button type="button" class="play-musique" value="' + musique["id_musique"] + '" onClick="playPauseFrom(\'' + musique["lien_musique"] + '\' )">' +
+              '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">'+
+                '<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>'+
+                '<path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>'+
+              '</svg>' +
+            '</button>' 
+
+        )
 
         $(".play-musique").click(function (event) {
             let id = $(event.target).closest('.play-musique').attr('value')
@@ -463,11 +488,17 @@ function showMusique(musique) {
 
 function loadPlaylistsMain(playlists){
     console.log(playlists)
-    $(".flex-page").html('<p>A quelle playlist souhaitez vous ajoutez '+playlists["musique"]["titre_musique"] +
-        ' de '+playlists["musique"]["pseudo_artiste"]+' ? </p>')
+    $(".flex-page").html(
+        '<h2>A quelle playlist souhaitez vous ajoutez '+playlists["musique"]["titre_musique"] +
+        ' de '+playlists["musique"]["pseudo_artiste"]+' ? </h2>'
+    )
     playlists["playlists"].forEach(function (playlist) {
         console.log(playlist)
-        $(".flex-page").append('<button class="add-playlist" value="' + playlist["id_playlist"] + '" type="submit">' + playlist["titre_playlist"] + '</button>')
+        $(".flex-page").append(
+        
+        '<button type="button" class="add-in-one-playlist" value="' + playlist["id_playlist"] + '" type="submit">' + playlist["titre_playlist"] + '</button>'
+        
+        )
     })
 
     $(".add-playlist").click(function (event) {
