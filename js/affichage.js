3637da50f1eb9f3a9ccdb7ab1338685e3f2dc849
iSlowz <mbellenger118@gmail.com>
2023-06-06 13:55:09 +0200

    p

--- js/affichage.js
+++ js/affichage.js
@@ -325,7 +325,7 @@ function showPlaylist(playlist) {    //affiche les musiques d'une playlist
         $(".play-musique").click(function (event) {
             let id = $(event.target).closest('.play-musique').attr('value')
             console.log(id);
-            ajaxRequest("POST", "request.php/historique/" + id + "?user="+id_user)
+            ajaxRequest("POST", "request.php/historique/" + id,()=>{},"id_user="+id_user)
         })
         $(".musique-bouton").click(function (event) {
             let id = $(event.target).closest('.musique-bouton').attr('value')   // id de la musique
@@ -404,7 +404,6 @@ function showMusique(musique) {
         
         if (musique["like"]===false) {
             $(".flex-page").append(
-                
                 '<button type="button" class="like-musique like-recherche-page" value="' + musique["id_musique"] + '">' + 
                   '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">' +
                     '<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>' +
@@ -464,7 +463,7 @@ function showMusique(musique) {
         $(".play-musique").click(function (event) {
             let id = $(event.target).closest('.play-musique').attr('value')
             console.log(id);
-            ajaxRequest("POST", "request.php/historique/" + id + "?user="+id_user)
+            ajaxRequest("POST", "request.php/historique/" + id,()=>{},"id_user="+id_user)
         })
         $("#add-playlist").click(function (event){
             let id = $(event.target).closest('#add-playlist').attr('value')   // id de la musique
